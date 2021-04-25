85086f6e4a67bb22a164cc275487c1949f4a3c23
Damien D <kerdelos@gmail.com>
2021-04-25 16:11:25 +0200

    added some cards

--- godot/game/gameplay/floppy_stat.gd
+++ godot/game/gameplay/floppy_stat.gd
@@ -3,7 +3,12 @@ extends Resource
 class_name FloppyStat
 
 export(String) var name
+export(String) var description
 export(int) var attack
+export(int) var breach
+export(int) var scan
+export(int) var move
+export(int) var ram
 export(int) var defense
 export(int) var cost
 export(int) var discard_cost = 1
