cd30784054b5aaacabb609fec560bc44d94d6bf2
Damien D <kerdelos@gmail.com>
2021-04-25 22:28:01 +0200

    wip sprite integration

--- godot/game/gameplay/net_node.gd
+++ godot/game/gameplay/net_node.gd
@@ -10,21 +10,54 @@ signal energy_bonus(amount)
 signal bin()
 
 
-enum netn_type {NONE, HOME, MISC, DATA, ENERGY, BIN, ENEMY, FIREWALL}
+enum netn_type {NONE, HOME, MISC, DATA, ENERGY, ENEMY, FIREWALL}
+enum netn_state {NONE, NEUTRAL, PLAYER, HERE}
 
-var node_type_to_color = {
-	netn_type.NONE: Color.darkred,
-	netn_type.HOME: Color.darkgreen,
-	netn_type.MISC: Color.gray,
-	netn_type.DATA: Color.darkblue,
-	netn_type.ENERGY: Color.darkorange,
-	netn_type.BIN: Color.violet,
+
+var sprites ={
+	netn_type.HOME :{
+		netn_state.NONE : [ preload("res://resources/gameplay/home_neutral_low.png"), preload("res://resources/gameplay/home_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/home_neutral_low.png"), preload("res://resources/gameplay/home_neutral_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/home_here_low.png"), preload("res://resources/gameplay/home_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/home_player_low.png"), preload("res://resources/gameplay/home_player_high.png")],
+	},
+	netn_type.MISC :{
+		netn_state.NONE : [ preload("res://resources/gameplay/unknown_neutral_low.png"), preload("res://resources/gameplay/unknown_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/node_neutral_low.png"), preload("res://resources/gameplay/node_neutral_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/node_here_low.png"), preload("res://resources/gameplay/node_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/node_player_low.png"), preload("res://resources/gameplay/node_player_high.png")],
+	},
+	netn_type.DATA :{
+		netn_state.NONE : [ preload("res://resources/gameplay/unknown_neutral_low.png"), preload("res://resources/gameplay/unknown_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/data_neutral_low.png"), preload("res://resources/gameplay/data_neutral_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/data_here_low.png"), preload("res://resources/gameplay/data_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/data_player_low.png"), preload("res://resources/gameplay/data_player_high.png")],
+	},
+	netn_type.ENERGY :{
+		netn_state.NONE : [ preload("res://resources/gameplay/unknown_neutral_low.png"), preload("res://resources/gameplay/unknown_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/cpu_neutral_low.png"), preload("res://resources/gameplay/cpu_neutral_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/cpu_here_low.png"), preload("res://resources/gameplay/cpu_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/cpu_player_low.png"), preload("res://resources/gameplay/cpu_player_high.png")],
+	},
+	netn_type.ENEMY :{
+		netn_state.NONE : [ preload("res://resources/gameplay/unknown_neutral_low.png"), preload("res://resources/gameplay/unknown_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/node_enemy_low.png"), preload("res://resources/gameplay/node_enemy_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/node_here_low.png"), preload("res://resources/gameplay/node_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/node_player_low.png"), preload("res://resources/gameplay/node_player_high.png")],
+	},
+	netn_type.FIREWALL :{
+		netn_state.NONE : [ preload("res://resources/gameplay/unknown_neutral_low.png"), preload("res://resources/gameplay/unknown_neutral_high.png")],
+		netn_state.NEUTRAL : [ preload("res://resources/gameplay/wall_neutral_low.png"), preload("res://resources/gameplay/wall_neutral_high.png")],
+		netn_state.PLAYER : [ preload("res://resources/gameplay/wall_here_low.png"), preload("res://resources/gameplay/wall_here_high.png")],
+		netn_state.HERE : [ preload("res://resources/gameplay/wall_player_low.png"), preload("res://resources/gameplay/wall_player_high.png")],
+	},
 }
 
 export (Array, NodePath) var out_nodes;
 var in_nodes = [];
 var connected_nodes = []
 
+var is_hovered = false;
 
 export var is_final_node = false;
 export (netn_type) var node_type;
@@ -32,11 +65,13 @@ export (netn_type) var node_type;
 export var is_player_controlled = false;
 var is_player_accessible = false;
 
+var current_state = netn_state.NONE;
+
 export (Array, Resource) var shop_content;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite.modulate = node_type_to_color[node_type];
+	refresh_sprite()
 	for o in out_nodes:
 		connected_nodes.append(get_node(o))
 	if is_player_controlled:
@@ -76,15 +111,15 @@ func on_acquired_by_player():
 		emit_signal("energy_bonus", 1)
 	elif node_type == netn_type.MISC:
 		pass
-	elif node_type == netn_type.BIN:
-		emit_signal("bin")
 
 func _on_Area2D_mouse_entered():
-	modulate = node_type_to_color[node_type].lightened(1.0)
+	is_hovered = true;
+	refresh_sprite()
 	emit_signal("netn_hovered",self)
 
 func _on_Area2D_mouse_exited():
-	modulate = node_type_to_color[node_type]
+	is_hovered = false;
+	refresh_sprite()
 	emit_signal("netn_unhovered",self)
 
 func receive_flop(flop_stat):
@@ -97,3 +132,8 @@ func can_receive_flop(flop_stat):
 	if is_player_accessible and not is_player_controlled:
 		return true;
 	return false
+
+func refresh_sprite():
+	if sprites.has(node_type):
+		var sprite_set = sprites[node_type]
+		$Sprite.texture = sprite_set[current_state][1] if is_hovered else sprite_set[current_state][0];
