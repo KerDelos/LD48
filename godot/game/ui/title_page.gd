140285334739d885ff41963dd673d1221ae3ff27
Damien D <kerdelos@gmail.com>
2021-04-26 22:19:22 +0200

    title page

--- godot/game/ui/title_page.gd
+++ godot/game/ui/title_page.gd
@@ -11,14 +11,25 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_ExitDetect_mouse_entered():
+	$ExitHigh.visible = true
 
 
-func _on_Play_Button_pressed():
-	get_tree().change_scene("res://game/main_scene.tscn")
+func _on_ExitDetect_mouse_exited():
+	$ExitHigh.visible = false
 
 
-func _on_Exit_Button_pressed():
+func _on_ExitDetect_pressed():
 	get_tree().quit()
+
+
+func _on_PlayDetect_mouse_entered():
+	$PlayHigh.visible = true
+
+
+func _on_PlayDetect_mouse_exited():
+	$PlayHigh.visible = false
+
+
+func _on_PlayDetect_pressed():
+	get_tree().change_scene("res://game/main_scene.tscn")
